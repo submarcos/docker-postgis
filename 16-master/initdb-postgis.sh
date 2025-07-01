19dcbb018f621e0a62ce391769ae46b6433cfa29
Jean-Etienne Castagnede <jecastagnede@vivaldi.net>
2025-07-01 14:56:22 +0000

    fix

--- 16-master/initdb-postgis.sh
+++ 16-master/initdb-postgis.sh
@@ -15,6 +15,7 @@ for DB in template_postgis "$POSTGRES_DB"; do
 	echo "Loading PostGIS extensions into $DB"
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
+		CREATE EXTENSION IF NOT EXISTS postgis_raster;
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		-- Reconnect to update pg_setting.resetval
 		-- See https://github.com/postgis/docker-postgis/issues/288
